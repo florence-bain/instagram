126358e8c02bbe4711e139b81013c53b21e3cbd9
Florence Bain <florence_bain@outlook.com>
2021-10-16 16:19:51 +0100

    add devise gem for sign in and sign out

---
+++ test/features/home_spec.rb
@@ -0,0 +1,9 @@
+# require "test_helper"
+
+# feature 'Home' do
+#   scenario 'Shows user the home page' do
+#     visit('/')
+#     click_button "Sign up"
+#     expect(page).to have_content 'Sign up'
+#   end
+# end
